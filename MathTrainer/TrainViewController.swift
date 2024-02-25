4e11e730901046c5587c2e617fa96e6646849383
balandzin <balandzin2602@gmail.com>
2024-02-25 12:48:39 +0300

    Тень кнопок на втором viewController

--- MathTrainer/TrainViewController.swift
+++ MathTrainer/TrainViewController.swift
@@ -9,8 +9,25 @@ import UIKit
 
 final class TrainViewController: UIViewController {
 
-    //MARK: - Properties
+    //MARK: - IBOutlets
+    @IBOutlet var buttonCollection: [UIButton]!
     
+    //MARK: - Properties
     var type: MathTypes = .add
     
+    //MARK: - Lifecycles
+    override func viewDidLoad() {
+        configureButton()
+    }
+    
+    // MARK: - Methods
+    private func configureButton() {
+        // Add shadow
+        buttonCollection.forEach { button in
+            button.layer.shadowColor = UIColor.darkGray.cgColor
+            button.layer.shadowOffset = CGSize(width: 0, height: 2)
+            button.layer.shadowOpacity = 0.4
+            button.layer.shadowRadius = 3
+        }
+    }
 }
